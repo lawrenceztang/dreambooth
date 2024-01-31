aa49471e26f82b997c3164d101d9a053f6ac4917
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2024-01-31 14:30:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import zipfile
 
 MAX_IMAGES = 150
 
-training_script_url = "https://raw.githubusercontent.com/huggingface/diffusers/2fada8dc1b4859c0c5ed04423a3386cc9e31ddd9/examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py"
+training_script_url = "https://raw.githubusercontent.com/huggingface/diffusers/ba28006f8b2a0f7ec3b6784695790422b4f80a97/examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py"
 subprocess.run(['wget', '-N', training_script_url])
 orchestrator_script_url = "https://huggingface.co/datasets/multimodalart/lora-ease-helper/raw/main/script.py"
 subprocess.run(['wget', '-N', orchestrator_script_url])
